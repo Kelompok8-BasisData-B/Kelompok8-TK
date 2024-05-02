f6be9ba3b5a1a4077e2a214ad840ae5ee4496e45
fikririsyad <fikri.risyad10@gmail.com>
2024-05-02 10:58:21 +0700

    Finish front-end

--- tayangan/urls.py
+++ tayangan/urls.py
@@ -1,11 +1,16 @@
 from django.urls import path
-from tayangan.views import show_trailer, show_tayangan, show_ulasan
+from tayangan.views import (show_trailer, show_tayangan, show_hasil_pencarian_trailer, show_hasil_pencarian_tayangan,
+                            show_film, show_series, show_episode)
 
 app_name = 'tayangan'
 
 urlpatterns = [
-     path('', show_tayangan, name='show_tayangan'),
-     path('trailer/', show_trailer, name='show_trailer'),
-     path('ulasan/', show_ulasan, name='show_ulasan'),
+    path('', show_tayangan, name='show_tayangan'),
+    path('trailer/', show_trailer, name='show_trailer'),
+    path('hasil-pencarian-trailer/', show_hasil_pencarian_trailer, name='show_hasil_pencarian_trailer'),
+    path('hasil-pencarian-tayangan/', show_hasil_pencarian_tayangan, name='show_hasil_pencarian_trailer'),
+    path('film/', show_film, name='show_film'),
+    path('series/', show_series, name='show_series'),
+    path('episode/', show_episode, name='show_episode')
 ]
 
--- tayangan/views.py
+++ tayangan/views.py
@@ -7,5 +7,17 @@ def show_trailer(request):
 def show_tayangan(request):
     return render(request, 'daftar_tayangan.html')
 
-def show_ulasan(request):
-    return render(request, 'bagian_ulasan.html')
+def show_hasil_pencarian_trailer(request):
+    return render(request, 'hasil_pencarian_trailer.html')
+
+def show_hasil_pencarian_tayangan(request):
+    return render(request, 'hasil_pencarian_tayangan.html')
+
+def show_film(request):
+    return render(request, 'film.html')
+
+def show_series(request):
+    return render(request, 'series.html')
+
+def show_episode(request):
+    return render(request, 'episode.html')
